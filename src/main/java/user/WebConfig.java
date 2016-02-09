a7fcb5db39299e7d12c09503585556a31b45f066
rafal <rnajczuk@gmail.com>
2016-02-09 20:52:12 +0100

    #

---
+++ src/main/java/user/WebConfig.java
@@ -0,0 +1,18 @@
+package user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Rafał on 2016-02-09.
+ */
+@Controller
+public class WebConfig {
+
+    @RequestMapping(value="/login",method = RequestMethod.POST)
+    public String login(Model model){
+        return "login";
+    }
+}
